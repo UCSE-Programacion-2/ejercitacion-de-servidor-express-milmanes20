98f2c312f9a4000162a517dab6244251d287a8eb
Marco Castillo Cabrera <marco.cast.cab@gmail.com>
2026-06-06 16:31:35 -0300

    feat: ruta raiz. Closes #2

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express = require('express');
 
 const app = express();
@@ -15,5 +16,45 @@ const usuarios = [
 ];
 
 // TODO: Define tus rutas aquí
+const router = express.Router(); // Crea un router para manejar las rutas de la API
 
+// Rutas para productos
+router.get('/productos', (req, res) => {
+  //filtro cuando se ingresa un query string con el nombre de la categoría
+  const categoria = req.query.categoria;
+  if (categoria) {
+    const productosFiltrados = productos.filter((p) => p.categoria === categoria); // Filtra los productos por categoría si se proporciona el query string
+    res.json(productosFiltrados);
+  } else {
+    res.json(productos);
+  }
+});
+
+router.get('/usuarios', (req, res) => {
+  res.json(usuarios);
+});
+router.get('/usuarios', (req, res) => {
+  res.json(usuarios);
+});
+
+router.get('/usuarios/:id', (req, res) => {
+  // filtro cuando se ingresa un id por parámetro de ruta
+  const id = parseInt(req.params.id);
+  const usuario = usuarios.find((u) => u.id === id);
+  if (usuario) {
+    res.json(usuario);
+  } else {
+    res.status(404).json({ error: 'Usuario no encontrado.' });
+  }
+});
+
+router.get('/', (req, res) => {
+  res.send('Bienvenid@s a nuestro servidor Express!');
+});
+
+app.use('/', router); // Agrega el router a la aplicación, con el prefijo '/'
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'No se ha encontrado la ruta ingresada.' });
+});
 module.exports = app;
